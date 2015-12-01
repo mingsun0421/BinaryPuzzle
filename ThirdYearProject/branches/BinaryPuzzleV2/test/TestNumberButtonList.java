eaa5fb73533d6af81280945e2f85a4fe00af75ad
zaac003 <zaac003@204957d8-2a5c-40bd-bac5-7652c41fb519>
2015-12-01 23:26:55 +0000

    Update unit test classes' comment.

--- ThirdYearProject/branches/BinaryPuzzleV2/test/TestNumberButtonList.java
+++ ThirdYearProject/branches/BinaryPuzzleV2/test/TestNumberButtonList.java
@@ -3,15 +3,35 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import mingsun.finalyear.project.NumberButton;
+import mingsun.finalyear.project.NumberButtonList;
+/**
+ * This class tests NumberButtonList class.
+ * @author Ming
+ *
+ */
 public class TestNumberButtonList {
-
+	static NumberButtonList nbList;
+	static NumberButton nb;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		nb = new NumberButton(0);
+		nbList = new NumberButtonList();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddNumberButton() {
+		nbList.add(nb);
+		assertEquals(nb,nbList.getNumberButton(0));
+	}
+	@Test
+	public void testGetNumberButton() {
+		NumberButton temp = nbList.getNumberButton(0);
+		assertEquals(nb,temp);
+	}
+	@Test
+	public void testGetSize() {
+		assertEquals(1,nbList.getSize());
 	}
 
 }
